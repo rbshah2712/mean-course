c3a7d9cb616508e73a652b5a3e4faa56a821dfa4
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2024-08-15 23:21:23 -0400

    Login and Signup create backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 const app = express();
 
-mongoose.connect('mongodb+srv://rsanghvi2712:BD6JApFuJJG2OsOn@cluster0.owsw8yh.mongodb.net/node-angular?retryWrites=true')
+mongoose.connect('mongodb+srv://rsanghvi2712:BD6JApFuJJG2OsOn@cluster0.owsw8yh.mongodb.net/node-angular')
   .then(() => {
     console.log('Connected to database');
   })
@@ -31,5 +32,6 @@ app.use((req, res, next) => {
 
 
 app.use("/api/posts",postsRoutes);
+app.use("/api/user",userRoutes);
 
 module.exports = app;
